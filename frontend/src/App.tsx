dfb042e99f3bb8a5d2964a3d7f3b4e159a4db603
bernatsampera <b.sampera@gmail.com>
2025-10-17 09:28:12 +0200

    feat: init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import {useState} from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+      <button className="btn btn-secondary">
+        <span>{count}</span>
+      </button>
+    </>
+  );
+}
+
+export default App;
